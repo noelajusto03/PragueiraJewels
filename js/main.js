f302c1aedbe2d2f72783b0dcfdfa1ec36cc2f8a9
Noela Pereira <noelapj2003@gmail.com>
2026-06-24 19:41:46 +0200

    estructura Completada

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 const menuBtn = document.getElementById("menu-btn");
 const closeBtn = document.getElementById("close-btn");
 const sideMenu = document.getElementById("side-menu");
+const overlay = document.getElementById("overlay");
 
 menuBtn.addEventListener("click", () => {
     sideMenu.classList.add("active");
+    overlay.classList.add("active");
 });
 
 closeBtn.addEventListener("click", () => {
     sideMenu.classList.remove("active");
+    overlay.classList.remove("active");
+});
+
+overlay.addEventListener("click", () => {
+    sideMenu.classList.remove("active");
+    overlay.classList.remove("active");
 });
